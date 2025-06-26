dbade01ce6de6eb22e440a7cfeb4f4d0c40e3c9d
jeyasurya2126 <jeyasurya.muthuraman@crestclimbers.com>
2025-06-26 17:36:49 +0530

    Changes in RegisterDD  Routes

--- Backend/controllers/registerController.js
+++ Backend/controllers/registerController.js
@@ -2,6 +2,10 @@
 const { BatchName , Department , PassedOutYear , Gender ,BloodGroup , UserType  } = require('../models');
 
 
+
+// BatchName 
+
+
 exports. getBatchDetails = async ( req , res ) => {
     try {
         const batch = await BatchName.findAll()
@@ -10,6 +14,48 @@ exports. getBatchDetails = async ( req , res ) => {
         res.status(500).json({ status : 'error' , message:error.message})
     }
 }
+
+exports. addBatchDetails = async ( req , res ) => {
+    try {
+        const newBatch = await BatchName.create(req.body);
+        res.status(201).json({status :'success' , data:newBatch})
+    } catch (error) {
+        res.status(500).json({ status : 'error' , message:error.message})
+    }
+}
+
+exports. deleteBatchDetails = async ( req , res ) => {
+    try {
+        const { id } = req.params;
+        const deleted = await BatchName.destroy( { where : { id }})
+        if(deleted) {
+          res.json({ status: 'success', message: 'Batch deleted successfully' });
+        } else {
+           res.status(404).json({ status: 'error', message: 'Batch not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ status : 'error' , message:error.message})
+    }
+}
+
+
+exports. updateBatchDetails = async ( req , res ) => {
+    try {
+        const { id } = req.params;
+        const [updated] = await BatchName.update(req.body , { where : { id } } )
+        if(updated){
+            const updatedBatch = await BatchName.findOne( { where : { id } })
+            res.json({ status : 'success' , data : updatedBatch })
+        } else {
+        res.status(404).json({ status: 'error', message: 'Batch not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ status : 'error' , message:error.message})
+    }
+}
+
+
+//Department
 
 exports. getDepartment = async ( req , res ) => {
     try {
@@ -20,6 +66,51 @@ exports. getDepartment = async ( req , res ) => {
     }
 }
 
+
+exports. addDepartment = async ( req , res ) => {
+    try {
+        const newDept = await Department.create(req.body);
+        res.json( { status : 'success' , data : newDept })
+    } catch (error) {
+        res.status(500).json( { status : 'error' , message : error.message})
+    }
+}
+
+
+exports. deleteDepartment = async ( req , res ) => {
+    try {
+        const { id } = req.params;
+        const deleted = await Department.destroy( { where : { id }})
+        if(deleted){
+            res.json( { status : 'success' , message : 'Department Deleted Succesfully'})
+        } else {
+            res.status(404).json({ status: 'error', message: 'Department not found' });
+
+        }
+    } catch (error) {
+        res.status(500).json( { staus : 'error' , message : error.message})
+    }
+}
+
+
+exports .updateDepartment = async ( req , res ) => {
+    try {
+        const { id } = req.params;
+        const [updated] = await Department.update(req.body , { where : { id }})
+          if(updated){
+            const updatedDepartment = await Department.findOne( { where : { id } })
+            res.json({ status : 'success' , data : updatedDepartment })
+        } else {
+        res.status(404).json({ status: 'error', message: 'Department not found' });
+        }
+    } catch (error) {
+        res.status(500).json( { status : 'error' , message : error.message})
+    }
+}
+
+
+// PassedOutYear 
+
 exports. getPassedOutYear = async ( req , res ) => {
     try {
         const poy = await PassedOutYear.findAll()
@@ -29,6 +120,53 @@ exports. getPassedOutYear = async ( req , res ) => {
     }
 }
 
+
+exports. addPassedOutYear = async ( req , res ) => {
+    try {
+        const newPoy = await PassedOutYear.create(req.body)
+        res.json( { status : 'success' , data : newPoy})
+    } catch (error) {
+        res.status(500).json( { status: ' error' , message : error.message})
+    }
+}
+
+
+exports .deletePassedOutYear = async ( req , res ) => {
+    try {
+        const { id } = req.params;
+        const deleted = await PassedOutYear.destroy( { where : { id }})
+        if(deleted){
+            res.json( { status : 'success' , message : 'POY Deleted Succesfully'})
+        } else {
+            res.status(404).json({ status: 'error', message: 'POY not found' });
+
+        }
+    } catch (error) {
+        res.status(500).json( { status: ' error' , message : error.message})
+    }
+}
+
+
+exports. updatePassedOutYear = async ( req , res ) => {
+    try {
+        const { id } = req.params;
+        const [updated] = await PassedOutYear.update(req.body , { where : { id }})
+        if(updated) {
+            const updatePoy = await PassedOutYear.findOne( { where : { id } })
+            res.json({ status : 'success' , data : updatePoy })
+        } else {
+        res.status(404).json({ status: 'error', message: 'POY not found' });
+        }
+    } catch (error) {
+        res.status(500).json( { status: ' error' , message : error.message})
+
+    }
+}
+
+
+
+
+// Gender 
 exports. getGender = async ( req , res ) => {
     try {
         const gender = await Gender.findAll()
@@ -48,7 +186,7 @@ exports. getBloodGroup = async ( req , res ) => {
 }
 
 
-
+//UserType
 exports .getUserType = async (req , res) => {
 
     try {
--- Backend/routes/register.js
+++ Backend/routes/register.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const { getBatchDetails, getDepartment, getPassedOutYear, getBloodGroup, getGender, getUserType } = require('../controllers/registerController');
+const { getBatchDetails, getDepartment, getPassedOutYear, getBloodGroup, getGender, getUserType, addBatchDetails, updateBatchDetails, deleteBatchDetails, addDepartment, updateDepartment, deleteDepartment, addPassedOutYear, updatePassedOutYear, deletePassedOutYear } = require('../controllers/registerController');
 const router = express.Router();
+
+// GetRoutes
 
 
 router.get('/batch' , getBatchDetails);
@@ -9,5 +11,23 @@ router.get('/passedoutyear' , getPassedOutYear);
 router.get('/bloodgroup' , getBloodGroup);
 router.get('/gender' , getGender);
 router.get('/usertype' , getUserType);
+
+
+// PostRoutes
+router.post('/createbatch' , addBatchDetails);
+router.post('/createdept' , addDepartment);
+router.post('/createpoy' , addPassedOutYear);
+
+//Put Routes
+router.put('/batch/:id' , updateBatchDetails);
+router.put('/dept/:id', updateDepartment);
+router.put('/poy/:id' , updatePassedOutYear);
+
+//Delete Routes
+router.delete('/batch/:id' , deleteBatchDetails);
+router.delete('/dept/:id', deleteDepartment);
+router.delete('/poy/:id' , deletePassedOutYear);
+
+
 
 module.exports= router ; 
